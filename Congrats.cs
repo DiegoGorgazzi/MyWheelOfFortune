5ffdaddb6f8e5241702aed7c02ff071c49d44d10
Diego Gorgazzi <44508750+DiegoGorgazzi@users.noreply.github.com>
2020-02-05 19:36:23 -0800

    Add project files.

---
+++ Congrats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyWheelOfFortune
+{
+    public class Congrats
+    {
+        //FIELD
+       // public string FullName;
+
+        //CONSTRUCTOR
+        //public Congrats(string fullName)
+        //{
+        //    this.FullName = fullName;
+        //}
+
+        //METHOD
+        public static void SayCongrats(string FullName)
+        {
+            var index = FullName.IndexOf('-');
+            if (index == -1 && !string.IsNullOrWhiteSpace(FullName))
+                Console.WriteLine("Congratulations!");
+
+        }
+
+
+    }
+
+ 
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyWheelOfFortune
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // var Message = new Congrats("He-llo");
+            Congrats.SayCongrats("Hel-lo");
+
+            //Message.SayCongrats();
+
+            
+     
+        }
+    }
+
+ 
+}
